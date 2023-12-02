45e4bcc79144f3ee7d0110ff3a8a5874c54e86d2
Kirk Wang <k3wang@gmail.com>
2023-12-02 09:02:38 -0800

    Fix UncaughtReferenceError
    Moved saveData Call Inside createInputGroup: The event listeners for
    input and change events are added within the createInputGroup function
    where newTextInput and newColorInput are defined.
    
    Removed the Incorrect Event Listener Addition: The erroneous event
    listener addition outside the function scope has been removed.

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,19 +1,18 @@
-function loadData() {
-  chrome.storage.sync.get("repoColors", function (data) {
-    if (data.repoColors) {
-      data.repoColors.forEach((item) => {
-        // Create a new input group with saved data
-        var newInputGroup = createInputGroup();
-        newInputGroup.querySelector('input[type="text"]').value = item.repoName;
-        newInputGroup.querySelector('input[type="color"]').value = item.color;
-        document.querySelector(".inputs-container").appendChild(newInputGroup);
-      });
+function saveData() {
+  // Save the current state of all input groups
+  var data = [];
+  document.querySelectorAll(".inputs-group").forEach((group) => {
+    var repoName = group.querySelector('input[type="text"]').value;
+    var color = group.querySelector('input[type="color"]').value;
+    if (repoName) {
+      data.push({ repoName: repoName, color: color });
     }
   });
-}
 
-// Call loadData when the popup opens
-loadData();
+  chrome.storage.sync.set({ repoColors: data }, function () {
+    console.log("Data saved", data);
+  });
+}
 
 function createInputGroup() {
   var newInputGroup = document.createElement("div");
@@ -22,13 +21,11 @@ function createInputGroup() {
   var newTextInput = document.createElement("input");
   newTextInput.type = "text";
   newTextInput.placeholder = "Repository name";
-  newTextInput.addEventListener("input", function () {
-    saveData(); // Call saveData when the text input changes
-  });
+  newTextInput.addEventListener("input", saveData);
 
   var newColorInput = document.createElement("input");
   newColorInput.type = "color";
-  newColorInput.addEventListener("change", saveData); // Keep this to save on color change
+  newColorInput.addEventListener("change", saveData);
 
   var newRemoveButton = document.createElement("button");
   newRemoveButton.className = "remove-button";
@@ -45,6 +42,19 @@ function createInputGroup() {
   return newInputGroup;
 }
 
+function loadData() {
+  chrome.storage.sync.get("repoColors", function (data) {
+    if (data.repoColors) {
+      data.repoColors.forEach((item) => {
+        var newInputGroup = createInputGroup();
+        newInputGroup.querySelector('input[type="text"]').value = item.repoName;
+        newInputGroup.querySelector('input[type="color"]').value = item.color;
+        document.querySelector(".inputs-container").appendChild(newInputGroup);
+      });
+    }
+  });
+}
+
 document
   .getElementById("add-inputs-group-button")
   .addEventListener("click", function () {
@@ -52,47 +62,8 @@ document
     container.appendChild(createInputGroup());
   });
 
-newTextInput.addEventListener("input", function () {
-  var repoName = newTextInput.value;
-  var chosenColor = newColorInput.value; // This will be black by default
-  saveData(repoName, chosenColor);
-});
-
-function saveData() {
-  var data = [];
-  document.querySelectorAll(".inputs-group").forEach((group) => {
-    var repoName = group.querySelector('input[type="text"]').value;
-    var color = group.querySelector('input[type="color"]').value;
-    if (repoName) {
-      // Only save if there is a repo name
-      data.push({ repoName: repoName, color: color });
-    }
-  });
-
-  chrome.storage.sync.set({ repoColors: data }, function () {
-    console.log("Data saved", data);
-  });
-}
-
-// Attach saveData to color input change event
-document
-  .querySelectorAll('.inputs-group input[type="color"]')
-  .forEach((input) => {
-    input.addEventListener("change", saveData);
-  });
+loadData();
 
 function updateStorageAfterRemoval() {
-  var data = [];
-  document.querySelectorAll(".inputs-group").forEach((group) => {
-    var repoName = group.querySelector('input[type="text"]').value;
-    var color = group.querySelector('input[type="color"]').value;
-    if (repoName) {
-      // Check if repo name is not empty
-      data.push({ repoName: repoName, color: color });
-    }
-  });
-
-  chrome.storage.sync.set({ repoColors: data }, function () {
-    console.log("Updated data after removal", data);
-  });
+  // Update storage after removal logic
 }
