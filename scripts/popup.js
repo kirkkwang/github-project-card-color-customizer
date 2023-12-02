c91ae4d5c6dc17eced24d7cba83e641ba9e81442
Kirk Wang <k3wang@gmail.com>
2023-12-01 20:17:02 -0800

    Implement the frontend
    This is more or less how the interface will look.  The user should be
    able to add new repos and set a color for each one.  Nothing is hooked
    up yet, but the interface is there.

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,1 +1,39 @@
-console.log("This is a popup!")
+function createInputGroup() {
+  var newInputGroup = document.createElement("div");
+  newInputGroup.className = "inputs-group";
+
+  var newTextInput = document.createElement("input");
+  newTextInput.type = "text";
+  newTextInput.placeholder = "Repository name";
+
+  var newColorInput = document.createElement("input");
+  newColorInput.type = "color";
+  newColorInput.addEventListener("change", function () {
+    var repoName = newTextInput.value;
+    var chosenColor = newColorInput.value;
+    // Save the repo name and chosen color to storage
+    console.log("Saving:", repoName, chosenColor); // For testing
+  });
+
+  var newRemoveButton = document.createElement("button");
+  newRemoveButton.className = "remove-button";
+  newRemoveButton.innerHTML = "&times;"; // Using HTML entity for 'X'
+  newRemoveButton.addEventListener("click", function () {
+    newInputGroup.remove();
+  });
+
+  newInputGroup.appendChild(newTextInput);
+  newInputGroup.appendChild(newColorInput);
+  newInputGroup.appendChild(newRemoveButton);
+
+  return newInputGroup;
+}
+
+document
+  .getElementById("add-inputs-group-button")
+  .addEventListener("click", function () {
+    var container = document.querySelector(".inputs-container");
+    container.appendChild(createInputGroup());
+  });
+
+document.querySelector(".inputs-container").appendChild(createInputGroup()); // Add the initial input group
